b801ddfac994a0c2bdfad0a348ac902ad592ed76
upendrapratapbind <upbind027@gmail.com>
2025-01-06 01:25:54 +0530

    custom theme code

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:shop_app/utils/theme/custom_themes/text_theme.dart';
 
 class TAppTheme{
   TAppTheme._();
   static ThemeData  lightaTheme = ThemeData(
-useMaterial3: true,
-    fontFamily: 'Poppins',
+      useMaterial3: true,
+      fontFamily: 'Poppins',
       brightness: Brightness.light,
-    primaryColor: Colors.blue
-
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme
   );
 
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme
+  );
 
 }
